9ab83b54bc4424ab9ce54415f579a0b333c7f276
sgmikami03 <74947937+sgmikami03@users.noreply.github.com>
2021-03-10 00:58:20 +0900

    [add]registerのview作成
    [add]registerのview作成

---
+++ bbs/resources/views/auth/register.blade.php
@@ -0,0 +1,33 @@
+@extends('app')
+
+@section('title', '新規登録')
+
+@section('nav')
+  @include("nav", ['LinkCreation' => false])
+@endsection
+
+@section('content')
+
+<h2 id="page-title">新規登録</h2>
+
+<form>
+  <div class="form-group">
+    <label for="email">メールアドレス</label>
+    <input type="email" name="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="メールアドレス">
+  </div>
+  <div class="form-group">
+    <label for="name">ユーザーネーム</label>
+    <input type="text" name="name" class="form-control" id="name" placeholder="ユーザーネーム">
+  </div>
+  <div class="form-group">
+    <label for="password">パスワード</label>
+    <input type="password" name="password" class="form-control" id="password" placeholder="パスワード">
+  </div>
+
+  <button type="submit" class="btn btn-primary">Submit</button>
+  <!--google解放後
+  <button type="googleで登録" class="btn btn-danger">googleで登録</button>
+  -->
+</form>
+
+@endsection
