cfc9c3373833949a7d95511c142d2f62fb5a06e5
sgmikami03 <74947937+sgmikami03@users.noreply.github.com>
2021-03-10 15:15:08 +0900

    [add]favion追加
    [add]favicon追加

--- bbs/resources/views/app.blade.php
+++ bbs/resources/views/app.blade.php
@@ -5,6 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    <!--favicon-->
+    <link rel="shortcut icon" href={{asset('favicon.ico')}}>
+  
     <!--fontawsome-->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
     <!--bootstamps-->
