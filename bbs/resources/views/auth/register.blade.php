75c620f883818ea4e202fe35a345ebc954ab8715
sgmikami03 <74947937+sgmikami03@users.noreply.github.com>
2021-03-10 11:03:02 +0900

    [update]registerの細かい内容変更
    [update]registerの細かい内容変更
    
    action,route追加,pass確認忘れていたので追加

--- bbs/resources/views/auth/register.blade.php
+++ bbs/resources/views/auth/register.blade.php
@@ -10,21 +10,26 @@
 
 <h2 id="page-title">新規登録</h2>
 
-<form>
+<form method="POST" action="{{route('register')}}">
+  @csrf
   <div class="form-group">
     <label for="email">メールアドレス</label>
-    <input type="email" name="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="メールアドレス">
+    <input type="email" name="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="メールアドレス" required value="{{old('email')}}">
   </div>
   <div class="form-group">
     <label for="name">ユーザーネーム</label>
-    <input type="text" name="name" class="form-control" id="name" placeholder="ユーザーネーム">
+    <input type="text" name="name" class="form-control" id="name" placeholder="ユーザーネーム" required value="{{old('name')}}">
   </div>
   <div class="form-group">
     <label for="password">パスワード</label>
-    <input type="password" name="password" class="form-control" id="password" placeholder="パスワード">
+    <input type="password" name="password" class="form-control" id="password" placeholder="パスワード" required>
+  </div>
+  <div class="form-group">
+    <label for="password_confirmation">パスワード(確認)</label>
+    <input type="password" name="password_confirmation" class="form-control" id="password_confirmation" placeholder="パスワード(確認)" required>
   </div>
 
-  <a class="link" href="">loginはこちら</a>
+  <a class="link" href="{{ route('login') }}">loginはこちら</a>
   <br><br>
 
   <button type="submit" class="btn btn-primary">Submit</button>
