1ad754f28b6e8994f6bf42d508ec6f09f5569642
sgmikami03 <74947937+sgmikami03@users.noreply.github.com>
2021-03-12 12:36:25 +0900

    [update]postのroute追加,overview追加
    [update]postのroute追加,overview追加

--- bbs/resources/views/articles/create.blade.php
+++ bbs/resources/views/articles/create.blade.php
@@ -10,9 +10,15 @@
 
   <h2 class="text-muted">{{ Auth::user()->name }}の単語帳作成</h2>
 
-  <form action="" name="wordbook">
+  <form method="POST" action="{{ route('articles.store') }}" name="wordbook">
+    @csrf
+
     <label class="text-muted" for="title">タイトルを入力</label>
     <input type="text" name="title" class="form-control">
+    <br>
+
+    <label for="overview" class="text-muted">概要入力</label>
+    <textarea name="overview" cols="30" rows="2" class="form-control"></textarea>
     <br>
     
     <create-wordbook></create-wordbook>
