8e5dc3f05cdbef5cc841f36c9d9c154e040335af
sgmikami03 <74947937+sgmikami03@users.noreply.github.com>
2021-03-13 10:21:45 +0900

    [update]indexにnameとoverview表示
    [update]indexにnameとoverview表示

--- bbs/resources/views/articles/card.blade.php
+++ bbs/resources/views/articles/card.blade.php
@@ -1,7 +1,8 @@
 <div class="card">
     <div class="card-body">
         <h5 class="card-title">{{ $article->title }}</h5>
-        <p class="card-text">投稿者:{{ var_dump($article->user) }}</p>
+        <p class="card-text">投稿者:{{ $article->user->name }}</p>
+        <p class="card-text">概要:{{ $article->overview }}</p>
         <p><a href="#" class="btn btn-light">詳細</a></p>
     </div>
 </div>
