29af2db4c1396f63bde6d28005ce7e01a6817f31
sgmikami03 <74947937+sgmikami03@users.noreply.github.com>
2021-03-13 13:25:26 +0900

    [add]editコントローラー操作追加、editのroute追加
    [add]editコントローラー操作追加、editのroute追加

--- bbs/app/Http/Controllers/ArticleController.php
+++ bbs/app/Http/Controllers/ArticleController.php
@@ -37,4 +37,9 @@ class ArticleController extends Controller
 
         return redirect()->route('index');
     }
+
+    public function edit(Article $article)
+    {
+        return view("articles.edit", ["article" => $article]);
+    }
 }
