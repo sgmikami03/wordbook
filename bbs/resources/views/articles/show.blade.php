a92e0eed70b7af93a25bcc1e0735ffd34921e809
sgmikami03 <74947937+sgmikami03@users.noreply.github.com>
2021-03-21 11:40:30 +0900

    [update]回答数追加
    [update]回答数追加

--- bbs/resources/views/articles/show.blade.php
+++ bbs/resources/views/articles/show.blade.php
@@ -10,9 +10,11 @@
 
   <h2 class="text-muted"><small>タイトル:</small>{{ $article->title }}</h2>
 
-  <h4 class="text-muted"><small>概要:</small>{{ $article->overview }}</h4>
+  <h5 class="text-muted"><small>概要:</small>{{ $article->overview }}</h5>
 
-  <h4 class="text-muted"><small>作成者:</small>{{ $article->user->name }}</h4>
+  <h5 class="text-muted"><small>回答数:</small>{{ $article->frequency }}</h5>
+
+  <h5 class="text-muted"><small>作成者:</small>{{ $article->user->name }}</h5>
 
   <a href="{{ route('articles.solve', ['article' => $article, 'isEnglishToJapanese' => 1]) }}" class="btn btn-light">問題を解く(英語→日本語)</a>
   <a href="{{ route('articles.solve', ['article' => $article, 'isEnglishToJapanese' => 0]) }}" class="btn btn-light">問題を解く(日本語→英語)</a>
